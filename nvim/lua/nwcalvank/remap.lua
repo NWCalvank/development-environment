c1413c29c68ea426db5db8216583af5d89990056
Nathan Calvank <nwcalvank@gmail.com>
2023-02-21 16:01:01 -0800

    Add Terminal Mode Remaps (#10)
    This should make terminal ergonomic enough for me to actually use it

--- nvim/lua/nwcalvank/remap.lua
+++ nvim/lua/nwcalvank/remap.lua
@@ -12,3 +12,9 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- Split
 vim.keymap.set("n", "<leader>ss", vim.cmd.split)
 vim.keymap.set("n", "<leader>vv", vim.cmd.vsplit)
+
+-- Terminal
+vim.keymap.set("n", "<leader>t", [[:split<enter>:terminal<enter>A]])  -- enter terminal split
+vim.keymap.set("n", "<leader>T", [[:vsplit<enter>:terminal<enter>A]]) -- enter terminal vsplit
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])                          -- leave terminal mode
+vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>:q<enter>]])            -- leave & close terminal
